157da60ae67c9e09129c7ba3cd01b73c3e904623
Irakli Adamashvili <adamashvili.irakli8@gmail.com>
2023-04-19 16:12:58 +0400

    finish responsive and dinamic navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,28 @@ import { MenuItems } from "./MenuItems";
 
 
 class Navbar extends Component {
+    state = {clicked: false};
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
     render() {
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">Trippy</h1>
+                <div className="menu-icons" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                    
+                </div>
 
-                <ul className="nav-menu">
+                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={index}>
-                        <a href="/"><i className={item.icon}></i> {item.title}</a>
+                        <a className={item.Cname} href="/"><i className={item.icon}></i> {item.title}</a>
                     </li>
                         )
                     })}
-                    
+                    <button>Sign Up</button>
                 </ul>
             </nav>
         )
